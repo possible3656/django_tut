4cf194ed9f787d0ee84fb42f12938f878a9ce338
sambhav <sj94611@gmail.com>
2023-01-17 22:56:28 +0530

    Style: added comments

--- demo1/urls.py
+++ demo1/urls.py
@@ -2,9 +2,14 @@
 from django.contrib import admin
 from django.urls import path
 from . import view
+# . means look in same directory as current file 
+# we are importing view to get all the function from it 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # we created a endpoint and assigned it a view 
+    # when its called that view is returned
     path('about/',view.about),
+    # empty string denotes homepage 
     path('',view.home),
 ]
--- demo1/view.py
+++ demo1/view.py
@@ -1,4 +1,7 @@
 from django.http import HttpResponse
+#go to urls.py
+#make a url then make a view for it 
+# below two are view which are returning simple text
 def about(request):
     return HttpResponse('about page')
     
