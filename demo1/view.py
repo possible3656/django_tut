53ab9bb396901497ac03d53db3f5aea417ba7bdb
sambhav <sj94611@gmail.com>
2023-01-17 23:12:58 +0530

    added html response using render

--- demo1/view.py
+++ demo1/view.py
@@ -2,8 +2,12 @@ from django.http import HttpResponse
 #go to urls.py
 #make a url then make a view for it 
 # below two are view which are returning simple text
+from django.shortcuts import render
+
 def about(request):
-    return HttpResponse('about page')
+   # return HttpResponse('about page')
+   return render(request,'about.html')
     
 def home(request):
-    return HttpResponse('home page')
+   # return HttpResponse('home page')
+   return render(request,'homepage.html')
